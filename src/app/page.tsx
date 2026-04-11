b2d29a52c35f1fa5e92b1adf7585da0449895f94
HarryFoster48 <harryjfoster47@gmail.com>
2026-04-11 14:21:36 +0100

    Create components and add task state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { Task } from "./types/task";
 
 export default function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
   return (
     <main className="flex min-h-screen items-center justify-center bg-black">
       <h1 className="text-3xl font-bold">Task Manager</h1>
+
+      <div className="w-full max-w-md">
+        <p className="text-white mb-4">Tasks here</p>
+      </div>
     </main>
   );
 }
